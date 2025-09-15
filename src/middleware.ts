aa6f4f2404068d0bbfc9dc611df01246a31a766e
Blaze Blizzaro <blazeblizzaro@gmail.com>
2025-09-15 22:35:27 +0000

    and i was directly into the chat as alice no login

--- src/middleware.ts
+++ src/middleware.ts
@@ -15,7 +15,13 @@ export function middleware(request: NextRequest) {
   }
 
   if (!authToken) {
-    return NextResponse.redirect(new URL('/login', request.url));
+    let url = new URL('/login', request.url);
+    // if there's an old token, we must clear it on the /logout route
+    // before allowing the user to see the /login page
+    if (request.cookies.has('auth-token')) {
+        url = new URL('/logout', request.url);
+    }
+    return NextResponse.redirect(url);
   }
 
   return NextResponse.next();
