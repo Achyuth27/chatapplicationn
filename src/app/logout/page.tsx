181b90f5f9a5a8fd40ba3f3d2675a4d88de002da
Blaze Blizzaro <blazeblizzaro@gmail.com>
2025-09-15 22:34:39 +0000

    ./src/app/logout/page.tsx (6:16)
    Ecmascript file had an error
       4 | im

--- src/app/logout/page.tsx
+++ src/app/logout/page.tsx
@@ -3,7 +3,7 @@
 import { cookies } from 'next/headers';
 import { redirect } from 'next/navigation';
 
-export default function LogoutPage() {
+export default async function LogoutPage() {
   cookies().delete('auth-token');
   redirect('/login');
 }
